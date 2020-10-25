5de8d74bd0834426456e289ad5ec17ef9b06f9e7
Nicholas Merolle <nmerolle11@hotmail.com>
2020-10-25 04:07:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,7 @@ class Triangle
   def kind()
     if a <= 0 || b <= 0|| c<=0
       raise TriangleError
-    elsif
-      a+b <= c|| a+c <= b|| b+c <= a
+    elsif a+b <= c|| a+c <= b|| b+c <= a
       
 
 
