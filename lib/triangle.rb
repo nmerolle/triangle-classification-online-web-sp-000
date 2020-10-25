a1ecf389e8e931e5f83ea41fb123393943057708
Nicholas Merolle <nmerolle11@hotmail.com>
2020-10-25 04:18:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,12 +9,13 @@ class Triangle
   def kind()
     if a <= 0 || b <= 0|| c<=0
       raise TriangleError
-    elsif a+b <= c|| a+c <= b|| b+c <= a
+    elsif (a+b <= c)|| (a+c <= b)|| (b+c <= a)
       raise TriangleError
     else
       if a == b && b == c
         :equilateral
-        elsif a == b||b == c|| a == c
+      elsif (a == b)||(b == c)|| (a == c)
+        :
       
       
       
